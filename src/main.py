1d70e951cd112f607c681d877915b1f7f26642d3
qw0te <baert.quentin@gmail.com>
2015-12-28 18:58:41 +0100

    Ajoute une fonction afin d'obtenir l'ordre des documents par rapport à leur distance à la requête

--- src/main.py
+++ src/main.py
@@ -59,6 +59,18 @@ def get_cos_distance(doc, q):
 
     return np.dot(doc, q) / (np.linalg.norm(doc, 2) * np.linalg.norm(q, 2))
 
+def get_order(docs, q):
+    """
+    Donne les documents dans l'ordre décroissant de leur distance à la requête.
+    """
+
+    res = {}
+
+    for i, doc in enumerate(docs.T):
+        res["d" + str(i + 1)] = get_cos_distance(doc, q)
+
+    return map(lambda couple: couple[0], sorted(res.items(), key=lambda couple: couple[1], reverse=True))
+
 def main():
     q, docs = create_doc_matrix(parse_doc("data/documents.txt"), query)
 
